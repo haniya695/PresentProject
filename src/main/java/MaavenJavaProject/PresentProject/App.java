b9d5dda355963fcd78e55bfdf842d90522a5f9cc
haniya <fatimahaniya016@gmail.com>
2024-11-24 13:52:55 +0530

    Demo For Test

---
+++ src/main/java/MaavenJavaProject/PresentProject/App.java
@@ -0,0 +1,14 @@
+package MaavenJavaProject.PresentProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println( "Haniya Fatima-22BD1A664R" );
+    }
+}
